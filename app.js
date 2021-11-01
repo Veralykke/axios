3d262c49beb3c9583c74e474e36cc6b1a6bf73b4
Vera Lykke Kaasen <55652971+Veralykke@users.noreply.github.com>
2021-11-01 15:48:37 +0100

    Adding clickevent

--- app.js
+++ app.js
@@ -22,11 +22,20 @@ const fetchBitcoinPrice = async () => {
 //NBNB: Here we only want application JSON, you have to read the DOCS to figure it out, example som want you to have and enpoint that has /json in it or add the query string in i like ?type=json..this API wants
 //a header, Accept set to application/json.."if we want json"
 const jokes = document.querySelector("#jokes");
+const button = document.querySelector('button');
+
+const addNewJoke = async () => {
+  const textJoke = await getDadJoke();
+  const newLI = document.createElement('LI');
+  newLI.append(textJoke);
+  jokes.append(newLI);
+}
+
 const getDadJoke = async () => {
   ///use async so we dont need to use .then osv
   const config = { headers: { Accept: "application/json" } };
   const res = await axios.get("https://icanhazdadjoke.com/", config);
-  const newLI = document.createElement('LI');
-  newLI.append(res.data.joke);
-  jokes.append(newLI);
-};
+ return res.data.joke;
+}
+
+button.addEventListener('click', addNewJoke )
