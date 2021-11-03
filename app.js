b51413518d84c794040d4259c8a082e41e1f0826
Vera Lykke Kaasen <55652971+Veralykke@users.noreply.github.com>
2021-11-03 11:32:54 +0100

    search form

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-/*axios
+const { default: axios } = require("axios");
+
+axios
+
+  ////////////////CLICK AND GET A NEW JOKE
   .get("https://api.cryptonator.com/api/ticker/btc-usd")
   .then((res) => {
     console.log(res.data.ticker.price);
   })
   .catch((err) => {
     console.log("ERROR", err);
-  });*/
+  });
 
 const fetchBitcoinPrice = async () => {
   try {
@@ -22,25 +26,33 @@ const fetchBitcoinPrice = async () => {
 //NBNB: Here we only want application JSON, you have to read the DOCS to figure it out, example som want you to have and enpoint that has /json in it or add the query string in i like ?type=json..this API wants
 //a header, Accept set to application/json.."if we want json"
 const jokes = document.querySelector("#jokes");
-const button = document.querySelector('button');
+const button = document.querySelector("button");
 
 const addNewJoke = async () => {
   const textJoke = await getDadJoke();
-  const newLI = document.createElement('LI');
+  const newLI = document.createElement("LI");
   newLI.append(textJoke);
   jokes.append(newLI);
-}
+};
 
 const getDadJoke = async () => {
   ///use async so we dont need to use .then osv
   try {
-    const config = { headers: { Accept: "application/json" } };
+    const config = { headers: { Accept: "application/json" } }; // HEADERS
     const res = await axios.get("https://icanhazdadjoke.com/", config);
-   return res.data.joke;
-  } catch  (e) {
-    return "NO JOKES AVAILABLE SORRY :("
-}
-}
+    return res.data.joke;
+  } catch (e) {
+    return "NO JOKES AVAILABLE SORRY :(";
+  }
+};
 
+button.addEventListener("click", addNewJoke);
 
-button.addEventListener('click', addNewJoke )
+////////////////////////////////SEARCH TC SHOW
+const form = document.querySelector("#searchForm");
+form.addEventListener('submit', async function (e) {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+  const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
+  console.log(res.data);
+});
